55cdc64a3ea0f1b03c4504a0c20fec05a2fec9e2
SabrinaPrestes <sabrinaprestes25@gmail.com>
2025-04-19 13:19:53 -0300

    Hello Spring

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String olaMundo() {
-        return "Hello World Spring!!";
+        return "Hello World Spring!";
 
     }
 }
